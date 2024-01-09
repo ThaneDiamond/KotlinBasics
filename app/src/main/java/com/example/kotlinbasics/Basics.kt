f6ad75f8d30a8238610cef4918bebf1b270adc90
Thane <86528978+Vetta54@users.noreply.github.com>
2024-01-09 16:48:48 +0200

    Commit and Push
    Created new project for Kotlin Basics

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasics
+
+
+fun main(){
+    val Number1 = 1
+    var String1 = "to"
+    println("Hello World! $Number1 $String1")
+    String1 = "too"
+    println("to changed to $String1")
+}
