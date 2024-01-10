933231c2464225c0f39d5583686aee04c0f2cac0
Thane <86528978+Vetta54@users.noreply.github.com>
2024-01-10 17:31:36 +0200

    Day 2: Boolean, floats and overall practice as well as an introduction to if/else statements and more logical operators

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -2,9 +2,43 @@ package com.example.kotlinbasics
 
 
 fun main(){
-    val Number1 = 1
+
+    val myChar = '#'
+    val myText = "###"
+    var myName = "Thane"
+
+
+    println("I'm a Char $myChar")
+    println("And I'm a String $myText")
+    println("this is my name \n \t ${myName.uppercase()}")
+
+    println("Can you guess a word with 6 letters")
+
+    // we can also ask for an age and parse the given value using .toInt
+    val guessLength = readln().length
+    if (guessLength == 6){
+        println("Correct")
+
+    }
+    else{
+        println("not quite right")
+    }
+
+
+
+
+/*
+    val myTrue: Boolean = true;
+    val myFalse: Boolean = false;
+
+    println(myFalse || myTrue ) //True
+    println(myTrue && myFalse) //False
+    println(!myTrue) //False
+*/
+
+/*    val Number1 = 1
     var String1 = "to"
     println("Hello World! $Number1 $String1")
     String1 = "too"
-    println("to changed to $String1")
+    println("to changed to $String1")*/
 }
---
+++ app/src/main/java/com/example/kotlinbasics/BooleanPractice.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasics
+
+fun main() {
+    val myTrue = true
+    val myFalse = false
+
+    println(myTrue || myFalse) //True
+    println(myTrue && myFalse) //False
+    println(!myTrue) //False
+}
